bc7e3dc972ab33d04c6238aa20d76697f0bcddd0
Fadi Quader <fadi.quader@moxbank.com>
2022-07-21 01:05:07 +0800

    read version

---
+++ src/WorkspaceEnv.ts
@@ -0,0 +1,38 @@
+import {EOL} from 'os'
+import {spawn} from 'child_process'
+
+export class WorkspaceEnv {
+  private workspace: string
+  constructor(workspace: string) {
+    this.workspace = workspace
+  }
+
+  async run(command, args): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const child = spawn(command, args, {cwd: this.workspace})
+      let isDone = false
+      const errorMessages: any[] = []
+      child.on('error', error => {
+        if (!isDone) {
+          isDone = true
+          reject(error)
+        }
+      })
+      child.stderr.on('data', chunk => errorMessages.push(chunk))
+      child.on('exit', code => {
+        if (!isDone) {
+          if (code === 0) {
+            void resolve(null)
+          } else {
+            const error = new Error(
+              `${errorMessages.join(
+                ''
+              )}${EOL}${command} exited with code ${code}`
+            )
+            reject(error)
+          }
+        }
+      })
+    })
+  }
+}
