41191dcb0fd9464880352e9b07e44dbcf83f3b81
Fadi Quader <fadi.quader@moxbank.com>
2022-07-20 21:31:01 +0800

    test pr values

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,16 @@ async function run(): Promise<void> {
   const octokit = github.getOctokit(GITHUB_TOKEN)
   const {context} = github
   const pullRequest = context?.payload?.pull_request
+  const defaultBranch = pullRequest?.base.repo.default_branch
+  const currentBranch = pullRequest?.head.ref
+  const labels: string[] = pullRequest?.labels.map(label => label?.name)
   // eslint-disable-next-line no-console
-  console.log(JSON.stringify(pullRequest, null, 2))
+  console.log('defaultBranch: ', defaultBranch)
+  // eslint-disable-next-line no-console
+  console.log('currentBranch: ', currentBranch)
+  // eslint-disable-next-line no-console
+  console.log('labels: ', labels.join(', '))
+  // console.log(JSON.stringify(pullRequest, null, 2))
 }
 
 void run()
